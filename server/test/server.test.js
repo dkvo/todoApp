82ce98eaade4ea41474cd4f4cd4074c1e10c7d1e
dkvo <dangkhoanvo@gmail.com>
2018-05-04 03:06:44 -0700

    test GET /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const  {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos  = [{
+    _id: new ObjectID(),
     text: 'first todo'
 }, {
+    _id: new ObjectID(),
     text: 'second todo'
 }];
 
@@ -64,4 +67,31 @@ describe('GET /todos', () => {
           })
           .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return a todo doc', (done) => {
+        request(app)
+          .get(`/todos/${todos[0]._id.toHexString()}`)
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todo.text).toBe(todos[0].text);
+          })
+          .end(done);
+
+    });
+    it('should return 404 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+        request(app)
+          .get(`/todos/${hexID}`)
+          .expect(404)
+          .end(done);
+    });
+    it('should returm 400 for non-object ids', (done) => {
+        request(app)
+          .get('/todos/dfd123')
+          .expect(404)
+          .end(done);
+        
+    })
 });
